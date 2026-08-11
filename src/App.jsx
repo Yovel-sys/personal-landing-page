8c8e608b82a9b966fecd4ffd57b64c2a58832f34
yovel-sys <yovel777@gmail.com>
2026-08-11 14:17:13 +0300

    remove password gate

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Footer from "./components/Footer";
 import {Toaster} from "react-hot-toast";
-import PasswordGate from "./components/PasswordGate";
 import {Analytics} from "@vercel/analytics/react";
 
 //import backgroundImage from "./assets/background01.jpeg";
@@ -12,14 +11,13 @@ import {Analytics} from "@vercel/analytics/react";
 function App() {
   return (
     <>
-      <PasswordGate>
-        <Router>
-          <div className="min-h-screen bg-gray-50 flex flex-col">
-            <Navbar />
-            <Toaster position="top-center" reverseOrder={false} />
+      <Router>
+        <div className="min-h-screen bg-gray-50 flex flex-col">
+          <Navbar />
+          <Toaster position="top-center" reverseOrder={false} />
 
-            {/* אם אני רוצה רקע תמונה */}
-            {/* <main
+          {/* אם אני רוצה רקע תמונה */}
+          {/* <main
             className="flex-grow bg-no-repeat bg-cover bg-center bg-fixed"
             style={{
               backgroundImage: `url(${backgroundImage})`,
@@ -27,20 +25,19 @@ function App() {
               backgroundBlendMode: "overlay",
             }}
           > */}
-            {/* הרקע של האפליקציה */}
-            <main className="flex-grow bg-[#CBC2DF] px-4 py-8">
-              <div className="max-w-7xl mx-auto">
-                <Analytics />
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/projects" element={<Projects />} />
-                </Routes>
-              </div>
-            </main>
-            <Footer />
-          </div>
-        </Router>
-      </PasswordGate>
+          {/* הרקע של האפליקציה */}
+          <main className="flex-grow bg-[#CBC2DF] px-4 py-8">
+            <div className="max-w-7xl mx-auto">
+              <Analytics />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+              </Routes>
+            </div>
+          </main>
+          <Footer />
+        </div>
+      </Router>
     </>
   );
 }
